033d01649956a68e94cec86183269b79feebf45a
Michael <michael@unomena.com>
2013-03-12 11:55:51 +0200

    added error chekcing if eula does not exist

--- unobase/eula/models.py
+++ unobase/eula/models.py
@@ -7,7 +7,10 @@ from django.db import models
 
 class EULAManager(models.Manager):
     def latest_eula(self):
-        return self.get_query_set().order_by('-version')[0]
+        try:
+            return self.get_query_set().order_by('-version')[0]
+        except IndexError:
+            return None
 
 class EULA(models.Model):
     content = models.TextField()
