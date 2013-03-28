3e8095ecbfbdc6b144549f6a96b558cea7ad1513
Michael <michael@unomena.com>
2013-03-28 08:58:31 +0200

    fixed issue on zerodivionerror for calculating vote averages

--- unobase/poll/utils.py
+++ unobase/poll/utils.py
@@ -12,6 +12,9 @@ def get_poll_percentages(poll_answers):
         total_vote_counts += poll_answer.vote_count
     
     for poll_answer in poll_answers:
-        vote_count_averages.append(float('%0.2f' % ((poll_answer.vote_count / total_vote_counts) * 100)))
+        try:
+            vote_count_averages.append(float('%0.2f' % ((poll_answer.vote_count / total_vote_counts) * 100)))
+        except ZeroDivisionError:
+            vote_count_averages.append(0.0)
     
     return vote_count_averages
