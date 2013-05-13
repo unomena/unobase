6492c10469c8fe7c2626eae95dc4c8a63e9c3d45
Michael <michael@unomena.com>
2013-05-13 14:10:26 +0200

    upped version in setup.py

--- unobase/corporate_site/admin.py
+++ unobase/corporate_site/admin.py
@@ -7,10 +7,13 @@ from django.contrib import admin
 
 from unobase.corporate_site import models
 
+class PressReleaseAdmin(admin.ModelAdmin):
+    exclude = ('image_name',)
+
 admin.site.register(models.Article)
 admin.site.register(models.News)
 admin.site.register(models.Award)
-admin.site.register(models.PressRelease)
+admin.site.register(models.PressRelease, PressReleaseAdmin)
 admin.site.register(models.MediaCoverage)
 admin.site.register(models.Event)
 admin.site.register(models.Vacancy)
