deab8c66d7dc24298933a4d8d57f7d092fb68cb8
Michael <michael@unomena.com>
2013-05-13 14:01:02 +0200

    upped version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unobase',
-    version='1.0.8.beta',
+    version='1.0.9.beta',
     description='Unomena Base Django Application',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read(),
     author='Unomena',
