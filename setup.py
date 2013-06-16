2c78d1a169a3af6f15f22c718d63a13d541a87c6
Euan Jonker <euan@unomena.com>
2013-06-16 18:28:35 +0200

    Upped the version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unobase',
-    version='1.2.3.beta',
+    version='1.2.4.beta',
     description='Unomena Base Django Application',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read(),
     author='Unomena',
