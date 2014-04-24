1d0347d6f9039e7366385a30aca5a58b74c6979b
michael <michael@unomena.com>
2014-04-24 10:22:36 +0200

    added outboundemail modeladmin

--- unobase/email_tracking/admin.py
+++ unobase/email_tracking/admin.py
@@ -3,4 +3,10 @@ __author__ = 'michael'
 from django.contrib import admin
 from unobase.email_tracking import models
 
-admin.site.register(models.OutboundEmail)
+
+class OutboundEmailAdmin(admin.ModelAdmin):
+    list_display = ['user', 'subject', 'sent_timestamp']
+    list_filter = ['user__email', 'subject', 'sent_timestamp']
+
+
+admin.site.register(models.OutboundEmail, OutboundEmailAdmin)
--- unobase/email_tracking/models.py
+++ unobase/email_tracking/models.py
@@ -3,11 +3,18 @@ __author__ = 'michael'
 from django.db import models
 from unobase import settings as unobase_settings
 
+
 class OutboundEmail(models.Model):
-    user = models.ForeignKey(unobase_settings.AUTH_USER_MODEL, related_name='inbound_emails')
+    user = models.ForeignKey(
+        unobase_settings.AUTH_USER_MODEL,
+        related_name='inbound_emails'
+    )
     subject = models.CharField(max_length=250)
     message = models.TextField()
     sent_timestamp = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ['-sent_timestamp']
+
+    def __unicode__(self):
+        return u'%s | %s %s' % (self.user, self.subject, self.sent_timestamp)
