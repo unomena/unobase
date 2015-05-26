2a0941973404691597079d4aedba6790209b6db4
Euan Jonker <euan@unomena.com>
2015-05-26 10:34:13 +0200

    Changed version to be 1.3.12.cumulus.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='unobase',
-    version='1.3.12v',
+    version='1.3.12.cumulus.1',
     description='Unomena Base Django Application',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read(),
     author='Unomena',
