311592e3f1893a3428bf91b12978059040348594
Euan Jonker <euan@unomena.com>
2015-05-26 10:37:34 +0200

    Changed EULA content field to use RedactorTextField instead.

--- unobase/eula/models.py
+++ unobase/eula/models.py
@@ -5,28 +5,28 @@ Created on 06 Mar 2013
 '''
 from django.db import models
 
-from ckeditor.fields import RichTextField
+from unobase import fields
 
 class EULA(models.Model):
     title = models.CharField(max_length=255, default='')
-    
+
     def latest_version(self):
         try:
             return self.instances.all().order_by('-date_created')[0]
         except IndexError:
             return None
-    
+
     def __unicode__(self):
         return u'%s' % self.title
-    
+
 class EULAVersion(models.Model):
     eula = models.ForeignKey(EULA, related_name='instances')
-    content = RichTextField()
+    content = fields.RedactorTextField()
     version = models.CharField(max_length=255)
     date_created = models.DateField(auto_now_add=True)
-    
+
     class Meta:
         ordering = ['-date_created']
-    
+
     def __unicode__(self):
         return u'%s Version %s' % (self.eula, self.version)
